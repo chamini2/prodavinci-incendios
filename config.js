bd31f86bec20a896398c15526c3e4f13be7a23f7
Donato <donato1357@gmail.com>
2021-03-27 09:14:09 -0400

    casi listo

--- config.js
+++ config.js
@@ -8,7 +8,16 @@ var config = {
     title: 'Áreas Protegidas en fuego',
     subtitle: 'Dos décadas de incendios dentro de las Áreas Protegidas Naturales de Venezuela',
     byline: 'By a Digital Storyteller',
-    footer: 'Source: source citations, etc.',
+    // footer: "Creditos\
+    // <p>Direccion general: <a>Este pana</a></p>\
+    // <p>Direccion general: <a>Este pana</a></p>\
+    // <p>Direccion general: <a>Este pana</a></p>\
+    // <p>Direccion general: <a>Este pana</a></p>\
+    // <p>Direccion general: <a>Este pana</a></p>\
+    // <p>Direccion general: <a>Este pana</a></p>\
+    // <p>Direccion general: <a>Este pana</a></p>\
+    // <hr>\
+    // <p class='fecha'>CARACAS, 19 DE MARZO DE 2021</p>",
     chapters: [
         {
           id: 'sumario',
@@ -48,8 +57,6 @@ var config = {
           hidden: false,
           description: 
             " <p>Este es el intertitulo</p><p>Esto es algo&nbsp;</p>\
-              <img src='./vzla.jpg'>\
-              <img src='./vzla.jpg'>\
               <p>Y esto es otra cosa <a href='https://google.com'>con uns linea de hipervinculo</a>&nbsp;y no se que mas poner asi que ni modo esto se queda asi&nbsp;</p>",
           location: {
               center: [-66.55775, -0.36321],
@@ -78,7 +85,7 @@ var config = {
           id: 'sumario',
           alignment: 'infografia',
           hidden: false,
-          image: './pulitzer-center-logo.png',
+          image: './vzla.jpg',
           description: 
               "Leyenda de la infografia",
           location: {
@@ -106,50 +113,13 @@ var config = {
         },
         {
           id: 'sumario',
-          alignment: 'footer',
-          hidden: false,
-          description: 
-              "<p class='creditos'><a>Creditos</a></p>\
-              <p>Direccion general: <a>Este pana</a></p>\
-              <p>Direccion general: <a>Este pana</a></p>\
-              <p>Direccion general: <a>Este pana</a></p>\
-              <p>Direccion general: <a>Este pana</a></p>\
-              <p>Direccion general: <a>Este pana</a></p>\
-              <p>Direccion general: <a>Este pana</a></p>\
-              <p>Direccion general: <a>Este pana</a></p>\
-              <hr>\
-              <p class='fecha'>CARACAS, 19 DE MARZO DE 2021</p>",
-          location: {
-              center: [-66.55775, -0.36321],
-              zoom: 3.48,
-              pitch: 21.00,
-              bearing: 0
-          },
-          mapAnimation: 'flyTo',
-          rotateAnimation: false,
-          callback: '',
-          onChapterEnter: [
-              {
-              layer: 'limiteraisg',
-              opacity: 0.9999999,
-              duration: 5000
-              }
-          ],
-          onChapterExit: [
-              {
-              layer: 'limiteraisg',
-              opacity: 0
-              }
-          ]
-        },
-        {
-          id: 'sumario',
           alignment: 'texto-avanzado',
           hidden: false,
+          image: './pulitzer-center-logo.png',
           description: 
-              " <img src='./pulitzer-center-logo.png'>\
-                <hr><h3>Este es el autor</h3><hr>\
-                <p>Y esto es otra cosa <a href='https://google.com'>con uns linea de hipervinculo</a>&nbsp;y no se que mas poner asi que ni modo esto se queda asi&nbsp;</p>",
+              " <br><hr><br><h3>Este es el autor</h3><br><hr>\
+              <br><p>Y esto es otra cosa <a href='https://google.com'>con uns linea de hipervinculo</a>&nbsp;y no se que mas poner asi que ni modo esto se queda asi&nbsp;</p>\
+                <br><p>Y esto es otra cosa <a href='https://google.com'>con uns linea de hipervinculo</a>&nbsp;y no se que mas poner asi que ni modo esto se queda asi&nbsp;</p>",
           location: {
               center: [-66.55775, -0.36321],
               zoom: 3.48,
@@ -208,5 +178,21 @@ var config = {
               }
           ]
         },
+        // {
+        //     id: 'sumario',
+        //     alignment: 'footer',
+        //     title: 'Creditos',
+        //     description: 
+        //         "<p class='creditos'><a>Creditos</a></p>\
+        //         <p>Direccion general: <a>Este pana</a></p>\
+        //         <p>Direccion general: <a>Este pana</a></p>\
+        //         <p>Direccion general: <a>Este pana</a></p>\
+        //         <p>Direccion general: <a>Este pana</a></p>\
+        //         <p>Direccion general: <a>Este pana</a></p>\
+        //         <p>Direccion general: <a>Este pana</a></p>\
+        //         <p>Direccion general: <a>Este pana</a></p>\
+        //         <hr>\
+        //         <p class='fecha'>CARACAS, 19 DE MARZO DE 2021</p>",
+        //   },
     ]
 };
