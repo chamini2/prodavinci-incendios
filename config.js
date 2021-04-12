3b85f43e3064f831f92a077c43afde239362e105
Matteo Ferrando <matteo.ferrando2@gmail.com>
2021-04-12 13:20:47 -0400

    add line height

--- config.js
+++ config.js
@@ -328,41 +328,5 @@ var config = {
           rotateAnimation: false,
           callback: ''
         }
-        // {
-        //   id: 'texto-avanzado-example',
-        //   alignment: 'texto-avanzado',
-        //   hidden: false,
-        //   image: './pulitzer-center-logo.png',
-        //   description:
-        //     `
-        //         <br><hr><br><h3>Este es el autor</h3><br><hr>
-        //         <br><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,\
-        //         totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta\
-        //         sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia\
-        //         consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui \
-        //         dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, <a href='https://google.com'>sed quia non \
-        //         numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem</a>. Ut enim ad \
-        //         minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi\
-        //         minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi\
-        //         consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae \
-        //         minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi\
-        //         consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae \
-        //         minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi\
-        //         minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi\
-        //         consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae \
-        //         minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi\
-        //         minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi\
-        //         consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>\
-        //     `,
-        //   location: {
-        //       center: [-66.55775, -0.36321],
-        //       zoom: 3.48,
-        //       pitch: 21.00,
-        //       bearing: 0
-        //   },
-        //   mapAnimation: 'flyTo',
-        //   rotateAnimation: false,
-        //   callback: ''
-        // }
     ]
 };
